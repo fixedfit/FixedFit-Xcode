c3943ddd9cd55bec0eca0c54d2147951033dd1bf
SuperMario04 <buenrm1@unlv.nevada.edu>
2018-04-03 11:03:37 -0700

    added title for settings and new empty storyboard for finding users

---
+++ FixedFit/FixedFit/Controller/UserFinderVC.swift
@@ -0,0 +1,22 @@
+//
+//  UserFinderVC.swift
+//  FixedFit
+//
+//  Created by Esmeralda Salas on 4/3/18.
+//  Copyright © 2018 UNLV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class UserFinderVC: UIViewController{
+    
+    //Initialize variables used to finding users from multiple sources
+    var mode = ""
+    var viewTitle = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationItem.title = viewTitle
+    }
+ 
+    
+}
